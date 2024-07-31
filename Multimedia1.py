35f6241f9163a06e84501ac640a8d0ad962fb2e2
Ardi240503 <139232176+Ardi240503@users.noreply.github.com>
2024-07-31 11:49:06 +0000

    chapter1

---
+++ Multimedia1.py
@@ -0,0 +1,8 @@
+import pygame
+from PIL import Image
+import cv2
+import moviepy.editor as mp
+from pydub import AudioSegment
+import tkinter as tk
+
+print("✅ Semua pustaka telah terinstal dengan benar!")
